1ec521b5811ea600f81ddd5d244efbe31e5afff6
spookybart101 <bartoverduin@hotmail.nl>
2023-05-29 14:08:20 +0200

    Update solver.py
    solver now works

--- solver.py
+++ solver.py
@@ -4,39 +4,25 @@ from ortools.sat.python import cp_model
 # Load Data
 routes = pd.read_excel(r'C:\Users\BartO\Desktop\SDVSP\output.xlsx')
 costs = pd.read_excel(r'C:\Users\BartO\Desktop\SDVSP\costs_output.xlsx')
-
-
-
-print(costs)
+stores = routes.iloc[:, 0].tolist()
+maxVehicles = 5
 
 # Solve problem
-def solve_vrp(routes, costs):
-    stores = routes.iloc[:, 0].tolist()
+def solve_vrp(routes, costs, maxVehicles):
     num_stores = len(stores)
     num_routes = len(routes.columns)
-
-    print(routes)
-    print(num_routes)
-    print(range(num_routes))
-    print(range(num_stores))
 
     model = cp_model.CpModel()
     x = {}
     for i in range(num_routes):
         x[i] = model.NewBoolVar(f'x_{i}')
 
-    print(x)
     # Constraints
-    # Make sure every store is visited
-    '''
-    for j in range(num_stores):
-        model.Add(sum(x[i] * routes.iloc[j, i] for i in range(num_routes)) == 1)
-        print(j)
-    '''
     for j in range(num_stores):
         model.Add(sum(x[i] * routes.iloc[j, i] for i in range(num_routes)) >= 1)
 
     # Make sure we do not use more than the maximum vehicles
+    model.Add(sum(x[i] for i in range(num_routes)) <= maxVehicles)
 
     # Objective function: Minimize costs
     objective = sum(x[i] * costs.iloc[i,0] for i in range(num_routes))
@@ -47,34 +33,38 @@ def solve_vrp(routes, costs):
     status = solver.Solve(model)
 
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-        
         optimal_objective = solver.ObjectiveValue()
-        return optimal_objective
-        '''
+        
         optimal_routes = []
         for i in range(num_routes):
             if solver.Value(x[i]):
                 optimal_routes.append(i)
         
-        optimal_route_costs = [costs[i] for i in optimal_routes]
+        optimal_route_costs = [costs.iloc[i,0]  for i in optimal_routes]
+        optimal_vehicles = len(optimal_routes)
 
-        return optimal_objective, optimal_routes, optimal_route_costs
-        '''
+        return optimal_objective, optimal_routes, optimal_route_costs, optimal_vehicles
+        
 
     
-    return 500000
+    return 'inf', None, None, None
 
 
-# Example usage
-# optimal_objective, optimal_routes, optimal_route_costs = solve_vrp(routes, costs)
+# Start solver
+optimal_objective, optimal_routes, optimal_route_costs, optimal_vehicles = solve_vrp(routes, costs, maxVehicles)
 
-optimal_objective = solve_vrp(routes, costs)
-print(optimal_objective)
-
-'''
+# Give output
 if optimal_routes:
-    for route, cost in zip(optimal_routes, optimal_route_costs):
-        print(f"Route {route} is driven with cost {cost}")
+    print("The following routes are driven: ")
+    for route in optimal_routes:
+        print(f"{route}: ", end="")
+        store_indices = [i for i in range(len(stores)) if routes.iloc[i, route] == 1]
+        print(" > ".join([str(i) for i in store_indices]), end=" > 0\n")
+
+    print("Total costs:", optimal_objective)
+    print("Amount of vehicles:", optimal_vehicles)
 else:
     print("No feasible solution found.")
-'''
+
+
+
